dd97756a0eb8d9af3d4ed822002a812c2a04f5ce
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-05-11 08:40:24 +0700

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,32 +1,177 @@
-import { Suspense } from "react";
-import { GetServerSideProps } from "next";
+import { Suspense, useState } from "react";
 import Script from "next/script";
-import axios from "axios";
 import Head from "next/head";
 const formatDate = (str: string) => {
   const date = new Date(str);
   return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 };
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function Page(data: any) {
   const article = data.data;
-    useEffect(() => {
+  const [iframeHeight, setIframeHeight] = useState<number | null>(null);
+  useEffect(() => {
     try {
-       ((window as any).adsbygoogle = (window as any)?.adsbygoogle || [])?.push({});
+      //setTimeout(() => {
+        var qcImgDiv = document.getElementById("qcImg");
+
+        if (qcImgDiv && 0) {
+          var insElement = document.createElement("ins");
+          insElement.className = "adsbygoogle";
+          insElement.style.display = "block";
+          insElement.setAttribute("data-ad-client", "ca-pub-3619133031508264");
+          insElement.setAttribute("data-ad-slot", "2041680062");
+          insElement.setAttribute("data-ad-format", "auto");
+          insElement.setAttribute("data-full-width-responsive", "true");
+
+          // Chèn đối tượng ins vào thẻ div
+          qcImgDiv.appendChild(insElement);
+        }
+        var qcDivqc3 = document.getElementById("qcmgidgb3");
+        if (qcDivqc3) {
+          // Tạo một thẻ div mới để chứa script và amp-embed
+          var scriptContainer = document.createElement("div");
+
+          // Tạo thẻ div cho script
+          var scriptDiv = document.createElement("div");
+          scriptDiv.id = "M936538ScriptRootC1619516";
+          scriptContainer.appendChild(scriptDiv);
+
+          // Tạo thẻ script
+          var scriptTag = document.createElement("script");
+          scriptTag.src =
+            "https://jsc.adskeeper.com/d/a/dailynews.thongtinluat.com.1619516.js";
+          scriptTag.async = true;
+          //  scriptContainer.appendChild(scriptTag);
+
+          // Chèn scriptContainer vào thẻ div "qc"
+          qcDivqc3.appendChild(scriptContainer);
+          qcDivqc3.appendChild(scriptTag);
+        }
+     
+          // Giua bai mgid
+          // Chọn thẻ div có ID "qc"
+          var qcDiv = document.getElementById("qcmgidgb");
+          if (qcDiv) {
+            // Tạo một thẻ div mới để chứa script và amp-embed
+            var scriptContainer = document.createElement("div");
+
+            // Tạo thẻ div cho script
+            var scriptDiv = document.createElement("div");
+            scriptDiv.id = "M942715ScriptRootC1596350";
+            scriptContainer.appendChild(scriptDiv);
+
+            // Tạo thẻ script
+            var scriptTag = document.createElement("script");
+            scriptTag.src =
+              "https://jsc.adskeeper.com/d/a/dailynews.thongtinluat.com.1576110.js";
+            scriptTag.async = true;
+            //  scriptContainer.appendChild(scriptTag);
+
+            // Chèn scriptContainer vào thẻ div "qc"
+            qcDiv.appendChild(scriptContainer);
+            qcDiv.appendChild(scriptTag);        
+            
+          }
+        
+        
+      //}, 100);
+      // push ads
+
+      var ads = document.getElementsByClassName("adsbygoogle").length;
+      for (var i = 0; i < ads; i++) {
+        ((window as any).adsbygoogle =
+          (window as any)?.adsbygoogle || [])?.push({});
+      }
     } catch (err) {
-      console.log('err2222');
+      console.log("err2222");
     }
+
+    // get all iframe
+    const iframes = document.querySelectorAll("iframe");
+    iframes.forEach((iframe: HTMLIFrameElement) => {
+      if (iframe) {
+        if (iframe.src.includes("twitter")) {
+          iframe.style.height = window.innerWidth <= 525 ? "650px" : "827px";
+          iframe.style.display = "block";
+          iframe.style.width = window.innerWidth <= 525 ? "100%" : "550px";
+          iframe.style.margin = "0 auto";
+        } else if (iframe.src.includes("instagram")) {
+          iframe.style.height = window.innerWidth <= 525 ? "553px" : `${628}px`;
+          iframe.style.display = "block";
+          iframe.style.width = window.innerWidth <= 525 ? "100%" : "100%";
+          iframe.style.margin = "0 auto";
+        } else {
+          iframe.style.height = window.innerWidth <= 525 ? "250px" : "300px";
+          iframe.style.display = "block";
+          iframe.style.width = window.innerWidth <= 525 ? "100%" : "100%";
+          iframe.style.margin = "0 auto";
+        }
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const scriptElement = document.createElement("script");
+    scriptElement.src = `https://jsc.adskeeper.com/d/a/dailynews.thongtinluat.com.1576108.js?v=${Math.floor(
+      Math.random() * 1000
+    )}`;
+    scriptElement.async = true;
+
+    const scriptContainer = document.getElementById(
+      "M936538ScriptRootC1576108"
+    );
+    if (scriptContainer) {
+      scriptContainer.appendChild(scriptElement);
+    }
+
+    console.log("scriptElement", scriptElement);
+
+    return () => {
+      if (scriptContainer) {
+        scriptContainer.removeChild(scriptElement);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const scriptElement = document.createElement("script");
+    scriptElement.src = `https://nexvelar.digital/dist/dev_player.js?site=c3e93111-ae86-49dc-8b4b-b8400716999d?v=${Math.floor(
+      Math.random() * 1000
+    )}`;
+    scriptElement.async = true;
+
+    const scriptContainer = document.getElementById(
+      "player_dev"
+    );
+    if (scriptContainer) {
+      scriptContainer.appendChild(scriptElement);
+    }
+
+    console.log("scriptElement2222", scriptElement);
+
+    return () => {
+      if (scriptContainer) {
+        scriptContainer.removeChild(scriptElement);
+      }
+    };
   }, []);
   return (
     <>
       <Head>
         <title>{article.name}</title>
         <meta property="og:image" content={article.avatarLink} />
-        <meta property="og:title" content={article.name} />
+        <meta
+          property="og:title"
+          content={article.summary ? article.summary : article.name}
+        />
       </Head>
-      <Script id="gg-1" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-87TD51N1G8`} />
+      <Script
+        id="gg-1"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-87TD51N1G8`}
+      />
       <Script id="gg-2" strategy="lazyOnload">
         {`
           window.dataLayer = window.dataLayer || [];
@@ -35,32 +180,34 @@ export default function Page(data: any) {
           gtag('config', 'G-87TD51N1G8');
         `}
       </Script>
-     <Script
-  id="adsbygoogle-init"
-  strategy="afterInteractive"
-  crossOrigin="anonymous"
-  src= "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3619133031508264"/>
+      <Script
+        id="adsbygoogle-init"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3619133031508264"
+      />
       <main>
-        <Script src="/qcscript.js" />
-        
-        <div className="container-flu details">
-              <script async  src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3619133031508264"    crossOrigin="anonymous"   ></script>
-       <ins
-      className="adsbygoogle"     
-     style={{ display: 'block' }}
-     data-ad-client="ca-pub-3619133031508264"
-     data-ad-slot="1236418798"
-     data-ad-format="auto"
-     data-full-width-responsive="true"
-    />
+      
+        <div className="container-flu details">         
+          <ins
+            className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-3619133031508264"
+            data-ad-slot="4419959600"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          />
 
-          <h1>{article.name}</h1>    
+          <h1>{article.summary ? article.summary : article.name}</h1>
 
-           <div id="M936538ScriptRootC1576109"></div>
-          <script   src="https://jsc.adskeeper.com/d/a/dailynews.thongtinluat.com.1576109.js"   async  ></script>
           <p className="mb-4 text-lg">
             Posted: {formatDate(article.dateTimeStart)}
           </p>
+          <div id="player_dev"></div>
+          {/* <script
+            async
+            src="https://nexvelar.digital/dist/dev_player.js?site=a1341372-9eea-49ed-bda9-97d7c58c1305"
+          ></script> */}
           <Suspense fallback={<p>Loading ...</p>}>
             <article
               className="content"
@@ -68,31 +215,36 @@ export default function Page(data: any) {
             />
           </Suspense>
         </div>
+
         <div id="M936538ScriptRootC1576108"></div>
-        <script
-          src="https://jsc.adskeeper.com/d/a/dailynews.thongtinluat.com.1576108.js"
-          async
-        ></script>
+        {/* <script src="https://jsc.adskeeper.com/n/e/newspaper.thongtinluat.com.1596349.js?v=<%= Math.floor(Math.random() * 1000) %>"></script> */}
       </main>
     </>
   );
 }
 
-export const getServerSideProps: GetServerSideProps<any> = async ({
-  params,
-}) => {
-  try {
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
 
-    const response = await axios.get(
-      `${process.env.APP_API}/News/news-detail?id=${params?.slug?.slice(params?.slug?.lastIndexOf("-") + 1)}`
-    );
+export async function getStaticProps({ params }: { params: any }) {
+  try {
+    const slug = params?.slug;
+    const response = await fetch(
+      `${process.env.APP_API}/News/news-detail?id=${slug?.slice(
+        slug?.lastIndexOf("-") + 1
+      )}`
+    ).then((res) => res.json());
     return {
-      props: { data: response.data.data },
+      props: { data: response.data },
+      revalidate: 360000,
     };
   } catch (error) {
-    console.error("Error fetching data:", error);
     return {
-      props: { data: [] as any[] }, // Sử dụng any type cho data
+      props: { data: {} },
     };
   }
-};
+}
