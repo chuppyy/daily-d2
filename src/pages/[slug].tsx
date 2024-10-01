67997bbd4681ddad67c0c2679c7de58eade11a34
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-10-01 14:45:18 +0700

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,223 +1,178 @@
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import Script from "next/script";
 import Head from "next/head";
+import { useEffect } from "react";
+
 const formatDate = (str: string) => {
   const date = new Date(str);
   return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 };
 
-import { useEffect } from "react";
-
 export default function Page(data: any) {
   const article = data.data;
-  const [iframeHeight, setIframeHeight] = useState<number | null>(null);
+  const {
+    videoScriptSrc,
+    googleClientId,
+    googleClientSlotId,
+    googleAdSlot,
+    mgWidgetId1,
+    mgWidgetId2,
+    mgWidgetFeedId,
+    adsKeeperSrc,
+    googleTagId,
+  } = data.parameters;
+
+  // // QC video
+  // useEffect(() => {
+  //   const scriptElement = document.createElement("script");
+  //   scriptElement.src = `${videoScriptSrc}?v=${Math.floor(Math.random() * 1000)}`;
+  //   scriptElement.async = true;
+  //   const scriptContainer = document.getElementById("player_dev");
+  //   if (scriptContainer) {
+  //     scriptContainer.appendChild(scriptElement);
+  //   }
+  //   return () => {
+  //     if (scriptContainer) {
+  //       scriptContainer.removeChild(scriptElement);
+  //     }
+  //   };
+  // }, [videoScriptSrc]);
+
+/*
+useEffect(() => {
+  // New Script
+  const script = document.createElement("script");
+  script.src = `https://cdn.unibotscdn.com/player/mvp/player.js?v=${Math.floor(
+    Math.random() * 1000
+  )}`;
+  script.async = true;
+  document.head.appendChild(script);
+  // Ensure the script runs once the component mounts
+  const script2 = document.createElement("script");
+  script2.innerHTML = `
+      window.unibots = window.unibots || { cmd: [] };
+      unibots.cmd.push(function() { unibotsPlayer("boonovel.com_1703240626524") });
+  `;
+  const scriptContainer = document.getElementById("div-ub-boonovel.com_1703240626524")
+  if(scriptContainer) {
+    scriptContainer.appendChild(script2);
+  }
+  
+  // Cleanup function to remove the script when the component unmounts
+  return () => {
+    const div = document.getElementById("div-ub-boonovel.com_1703240626524");
+    if (div) {
+      div.innerHTML = "";
+    }
+  };
+}, []);*/
+
+
   useEffect(() => {
     try {
-      //setTimeout(() => {
-        var qcImgDiv = document.getElementById("qcImg");
+      var qcImgDiv = document.getElementById("qcImg");
+      if (qcImgDiv) {
+        var insElement = document.createElement("ins");
+        insElement.className = "adsbygoogle";
+        insElement.style.display = "block";
+        insElement.setAttribute("data-ad-client", googleClientId);
+        insElement.setAttribute("data-ad-slot", googleAdSlot);
+        insElement.setAttribute("data-ad-format", "auto");
+        insElement.setAttribute("data-full-width-responsive", "true");
+        qcImgDiv.appendChild(insElement);
+      }
 
-        if (qcImgDiv && 0) {
-          var insElement = document.createElement("ins");
-          insElement.className = "adsbygoogle";
-          insElement.style.display = "block";
-          insElement.setAttribute("data-ad-client", "ca-pub-3619133031508264");
-          insElement.setAttribute("data-ad-slot", "2041680062");
-          insElement.setAttribute("data-ad-format", "auto");
-          insElement.setAttribute("data-full-width-responsive", "true");
+      var qcDivqc3 = document.getElementById("qcmgidgb3");
+      if (qcDivqc3) {
+        var newDiv = document.createElement('div');
+        newDiv.innerHTML = `<div data-type="_mgwidget" data-widget-id="${mgWidgetId1}"></div>
+                            <script>(function(w,q){w[q]=w[q]||[];w[q].push(["_mgc.load"])})(window,"_mgq");</script>`;
+        qcDivqc3.appendChild(newDiv);
+      }
 
-          // Chèn đối tượng ins vào thẻ div
-          qcImgDiv.appendChild(insElement);
-        }
-        var qcDivqc3 = document.getElementById("qcmgidgb3");
-        if (qcDivqc3) {
-          // Tạo một thẻ div mới để chứa script và amp-embed
-          var scriptContainer = document.createElement("div");
+      var qcDiv = document.getElementById("qcmgidgb");
+      if (qcDiv) {
+        var newDiv = document.createElement('div');
+        newDiv.innerHTML = `<div data-type="_mgwidget" data-widget-id="${mgWidgetId2}"></div>
+                            <script>(function(w,q){w[q]=w[q]||[];w[q].push(["_mgc.load"])})(window,"_mgq");</script>`;
+        qcDiv.appendChild(newDiv);
+      }
 
-          // Tạo thẻ div cho script
-          var scriptDiv = document.createElement("div");
-          scriptDiv.id = "M936538ScriptRootC1619516";
-          scriptContainer.appendChild(scriptDiv);
-
-          // Tạo thẻ script
-          var scriptTag = document.createElement("script");
-          scriptTag.src =
-            "https://jsc.adskeeper.com/d/a/dailynews.thongtinluat.com.1619516.js";
-          scriptTag.async = true;
-          //  scriptContainer.appendChild(scriptTag);
-
-          // Chèn scriptContainer vào thẻ div "qc"
-          qcDivqc3.appendChild(scriptContainer);
-          qcDivqc3.appendChild(scriptTag);
-        }
-     
-          // Giua bai mgid
-          // Chọn thẻ div có ID "qc"
-          var qcDiv = document.getElementById("qcmgidgb");
-          if (qcDiv) {
-            // Tạo một thẻ div mới để chứa script và amp-embed
-            var scriptContainer = document.createElement("div");
-
-            // Tạo thẻ div cho script
-            var scriptDiv = document.createElement("div");
-            scriptDiv.id = "M942715ScriptRootC1596350";
-            scriptContainer.appendChild(scriptDiv);
-
-            // Tạo thẻ script
-            var scriptTag = document.createElement("script");
-            scriptTag.src =
-              "https://jsc.adskeeper.com/d/a/dailynews.thongtinluat.com.1576110.js";
-            scriptTag.async = true;
-            //  scriptContainer.appendChild(scriptTag);
-
-            // Chèn scriptContainer vào thẻ div "qc"
-            qcDiv.appendChild(scriptContainer);
-            qcDiv.appendChild(scriptTag);        
-            
-          }
-        
-        
-      //}, 100);
-      // push ads
-
-      var ads = document.getElementsByClassName("adsbygoogle").length;
+      const ads = document.getElementsByClassName("adsbygoogle").length;
       for (var i = 0; i < ads; i++) {
-        ((window as any).adsbygoogle =
-          (window as any)?.adsbygoogle || [])?.push({});
+        ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({});
       }
     } catch (err) {
-      console.log("err2222");
+      console.error("Error with ads", err);
     }
 
-    // get all iframe
+    // Adjust iframe dimensions
     const iframes = document.querySelectorAll("iframe");
     iframes.forEach((iframe: HTMLIFrameElement) => {
       if (iframe) {
         if (iframe.src.includes("twitter")) {
           iframe.style.height = window.innerWidth <= 525 ? "650px" : "827px";
-          iframe.style.display = "block";
           iframe.style.width = window.innerWidth <= 525 ? "100%" : "550px";
-          iframe.style.margin = "0 auto";
         } else if (iframe.src.includes("instagram")) {
-          iframe.style.height = window.innerWidth <= 525 ? "553px" : `${628}px`;
-          iframe.style.display = "block";
-          iframe.style.width = window.innerWidth <= 525 ? "100%" : "100%";
-          iframe.style.margin = "0 auto";
+          iframe.style.height = window.innerWidth <= 525 ? "553px" : "628px";
+          iframe.style.width = "100%";
         } else {
           iframe.style.height = window.innerWidth <= 525 ? "250px" : "300px";
-          iframe.style.display = "block";
-          iframe.style.width = window.innerWidth <= 525 ? "100%" : "100%";
-          iframe.style.margin = "0 auto";
+          iframe.style.width = "100%";
         }
       }
     });
-  }, []);
+  }, [googleClientId, googleAdSlot, mgWidgetId1, mgWidgetId2]);
 
-  useEffect(() => {
-    const scriptElement = document.createElement("script");
-    scriptElement.src = `https://jsc.adskeeper.com/d/a/dailynews.thongtinluat.com.1576108.js?v=${Math.floor(
-      Math.random() * 1000
-    )}`;
-    scriptElement.async = true;
-
-    const scriptContainer = document.getElementById(
-      "M936538ScriptRootC1576108"
-    );
-    if (scriptContainer) {
-      scriptContainer.appendChild(scriptElement);
-    }
-
-    console.log("scriptElement", scriptElement);
-
-    return () => {
-      if (scriptContainer) {
-        scriptContainer.removeChild(scriptElement);
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    const scriptElement = document.createElement("script");
-    scriptElement.src = `https://nexvelar.digital/dist/dev_player.js?site=c3e93111-ae86-49dc-8b4b-b8400716999d?v=${Math.floor(
-      Math.random() * 1000
-    )}`;
-    scriptElement.async = true;
-
-    const scriptContainer = document.getElementById(
-      "player_dev"
-    );
-    if (scriptContainer) {
-      scriptContainer.appendChild(scriptElement);
-    }
-
-    console.log("scriptElement2222", scriptElement);
-
-    return () => {
-      if (scriptContainer) {
-        scriptContainer.removeChild(scriptElement);
-      }
-    };
-  }, []);
   return (
     <>
       <Head>
-        <title>{article.name}</title>
+        <title>{article.name + "-" + article.userCode}</title>
         <meta property="og:image" content={article.avatarLink} />
-        <meta
-          property="og:title"
-          content={article.summary ? article.summary : article.name}
-        />
+        <meta property="og:title" content={article.name + "-" + article.userCode} />
       </Head>
-      <Script
-        id="gg-1"
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-87TD51N1G8`}
-      />
+      <Script src={adsKeeperSrc} async></Script>
+      <Script id="gg-1" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${googleTagId}`} />
       <Script id="gg-2" strategy="lazyOnload">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-87TD51N1G8');
+          gtag('config', '${googleTagId}');
         `}
       </Script>
-      <Script
-        id="adsbygoogle-init"
-        strategy="afterInteractive"
-        crossOrigin="anonymous"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3619133031508264"
-      />
+     
+      <Script id="adsbygoogle-init" strategy="afterInteractive" crossOrigin="anonymous" src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${googleClientId}`} />
+
       <main>
-      
-        <div className="container-flu details">         
+        <div className="container-flu details">
           <ins
             className="adsbygoogle"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-3619133031508264"
-            data-ad-slot="4419959600"
+            style={{ display: 'block' }}
+            data-ad-client={googleClientId}
+            data-ad-slot={googleClientSlotId}
             data-ad-format="auto"
             data-full-width-responsive="true"
           />
-
-          <h1>{article.summary ? article.summary : article.name}</h1>
-
-          <p className="mb-4 text-lg">
-            Posted: {formatDate(article.dateTimeStart)}
-          </p>
+          <h1>{article.name}</h1>
+          <p className="mb-4 text-lg">Posted: {formatDate(article.dateTimeStart)}</p>
           <div id="player_dev"></div>
-          {/* <script
-            async
-            src="https://nexvelar.digital/dist/dev_player.js?site=a1341372-9eea-49ed-bda9-97d7c58c1305"
-          ></script> */}
+          <div id="div-ub-boonovel.com_1703240626524"></div>
           <Suspense fallback={<p>Loading ...</p>}>
-            <article
-              className="content"
-              dangerouslySetInnerHTML={{ __html: article.content }}
-            />
+            <article className="content" dangerouslySetInnerHTML={{ __html: article.content }} />
           </Suspense>
         </div>
 
-        <div id="M936538ScriptRootC1576108"></div>
-        {/* <script src="https://jsc.adskeeper.com/n/e/newspaper.thongtinluat.com.1596349.js?v=<%= Math.floor(Math.random() * 1000) %>"></script> */}
+        <div data-type="_mgwidget" data-widget-id={mgWidgetFeedId}></div>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,q){w[q]=w[q]||[];w[q].push(["_mgc.load"])})
+              (window,"_mgq");
+            `,
+          }}
+          async
+        ></script>
       </main>
     </>
   );
@@ -233,18 +188,36 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }: { params: any }) {
   try {
     const slug = params?.slug;
-    const response = await fetch(
-      `${process.env.APP_API}/News/news-detail?id=${slug?.slice(
-        slug?.lastIndexOf("-") + 1
-      )}`
-    ).then((res) => res.json());
+    const response = await fetch(`${process.env.APP_API}/News/news-detail?id=${slug?.slice(slug?.lastIndexOf("-") + 1)}`).then((res) => res.json());
+
+    // Pass parameters dynamically
+    const parameters = {
+      videoScriptSrc: "https://nexvelar.digital/ads/nthotnews_boonovel_com.eb373146-0084-4675-83c9-23556caad088.video.js",
+      //Code auto 
+      googleClientId: "ca-pub-2388584177550957",
+      //GA tiêu đề
+      googleClientSlotId:"3774759985",
+      //GA sau video
+      googleAdSlot: "3774759985",
+//Cái sau
+      mgWidgetId1: "1677042",
+      //Cái trước
+      mgWidgetId2: "1677042",
+
+      mgWidgetFeedId: "1677041",
+      //scrip adkeeper
+      adsKeeperSrc: "https://jsc.adskeeper.com/site/986651.js",
+      //Analytic
+      googleTagId: "G-87TD51N1G8",
+    };
+
     return {
-      props: { data: response.data },
+      props: { data: response.data, parameters },
       revalidate: 360000,
     };
   } catch (error) {
     return {
-      props: { data: {} },
+      props: { data: {}, parameters: {} },
     };
   }
 }
